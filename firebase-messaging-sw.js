ffc8f317047a39bd32168dd73f0ceba21bb2197e
Vojtech Janota <vojtech.janota@hpe.com>
2017-02-23 15:32:09 +0100

    upgrade messaging version in worker

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,20 +1,9 @@
-importScripts('https://www.gstatic.com/firebasejs/3.5.2/firebase-app.js');
-importScripts('https://www.gstatic.com/firebasejs/3.5.2/firebase-messaging.js');
+importScripts('https://www.gstatic.com/firebasejs/3.6.9/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/3.6.9/firebase-messaging.js');
 
 firebase.initializeApp({
     'messagingSenderId': '739898137035'
 });
 
+// messaging must be started
 const messaging = firebase.messaging();
-
-// messaging.setBackgroundMessageHandler(function(payload) {
-//     console.log('[firebase-messaging-sw.js] Received background message ', payload);
-//     // const notificationTitle = '' + payload.data.stationCode + ' is ' + payload.data.stationIdx;
-//     const notificationTitle = JSON.stringify(payload);
-//     const notificationOptions = {
-//         body: notificationTitle
-//     };
-//
-//     return self.registration.showNotification(notificationTitle,
-//         notificationOptions);
-// });
